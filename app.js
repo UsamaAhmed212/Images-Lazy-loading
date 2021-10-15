6fb031633cf3a6cb449a332490ad52b5998f1854
Usama Ahmed <usamaahmed212aaa@gmail.com>
2021-10-15 20:52:29 +0600

    Images Lazy Loading Useing Custom js

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const images = document.querySelectorAll("img[data-src]");
-    
-    function loadImage(image) {
-        const src = image.getAttribute("data-src");
-        if(!src) {
-            return;
-        }
-        image.src  = src;
-        image.removeAttribute("data-src");
-    }
+    loadImage = function(e) {
+        var elements = document.querySelectorAll("img[data-src]");
+        elements.forEach(function (element) {
+            if(element.hasAttribute("data-src") && element.getAttribute("data-src")) {
+                var rect = element.getBoundingClientRect();
+                if (rect.top < window.innerHeight) {
+                    const viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+                    const viewportHeight = window.innerHeight || document.documentElement.clientHeight;
+                    const {top,bottom,left,right} = element.getBoundingClientRect();
+                    const isInViewport = top + element.clientHeight >= 0 &&
+                                        left + element.clientWidth >= 0 && 
+                                        bottom <= viewportHeight + element.clientHeight && 
+                                        right <= viewportWidth + element.clientWidth;
 
-    const imgOptions = {
-        threshold: 0,
-        rootMargin: '0px 0px 300px 0px'
-    };
-
-    const imgObserver = new IntersectionObserver((entries, observer) =>{
-        entries.forEach(function(entry) {
-            if(!entry.isIntersecting) {
-                return;
-            } else {
-                loadImage(entry.target);
-                imgObserver.unobserve(entry.target);
+                    if(isInViewport) {
+                        element.setAttribute("src", element.getAttribute("data-src"));
+                        element.removeAttribute("data-src");
+                    }
+                }
             }
+            
         });
-    }, imgOptions);
+    };
+    
+    window.addEventListener('load', loadImage);
+    window.addEventListener('scroll', loadImage);
+    document.querySelector("div").addEventListener('scroll', loadImage);
+    window.addEventListener('resize', loadImage);
+    window.addEventListener("wheel", loadImage);
 
-    images.forEach(function(image) {
-        imgObserver.observe(image);
-    });
 });
